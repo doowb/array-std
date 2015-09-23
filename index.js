a150da119cdbc90c2babd8f9e1d7f5f551ecdfd2
Brian Woodward <brian.woodward@gmail.com>
2015-09-23 09:48:28 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * array-std <https://github.com/doowb/array-std>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
